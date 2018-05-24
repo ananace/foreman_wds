924a89d92684907c190e70d117110dcd72eff1d8
Alexander Olofsson <alexander.olofsson@liu.se>
2018-05-24 16:06:26 +0200

    Add an endpoint to allow switching to local boot

--- app/controllers/concerns/foreman_wds/hosts_controller_extensions.rb
+++ app/controllers/concerns/foreman_wds/hosts_controller_extensions.rb
@@ -8,6 +8,28 @@ module ForemanWds
       render partial: 'wds_servers/image_select', locals: { item: wds_facet }
     end
 
+    def host_template
+      return super unless params[:kind] == 'wds_localboot'
+
+      iface = @host.provision_interface
+
+      # Set PXE template parameters
+      @kernel = @host.operatingsystem.kernel(@host.arch)
+      @initrd = @host.operatingsystem.initrd(@host.arch)
+      @mediapath = @host.operatingsystem.mediumpath(@host) if @host.operatingsystem.respond_to?(:mediumpath)
+
+      # Xen requires additional boot files.
+      @xen = @host.operatingsystem.xen(host.arch) if @host.operatingsystem.respond_to?(:xen)
+
+      iface.send :default_pxe_render, @host.operatingsystem.pxe_loader_kind(@host)
+
+      render inline: "Success. Local boot template was deployed successfully.\n"
+    rescue StandardError => e
+      message = format('Failed to set local boot template: %{error}', error: e)
+      logger.error message
+      render text: message, status: :error, content_type: 'text/plain'
+    end
+
     def host_params(top_level_hash = controller_name.singularize)
       # Don't create a WDS facet unless provisioning with it
       params[:host].delete :wds_facet_attributes if params[:host] && params[:host][:provision_method] != 'wds'
