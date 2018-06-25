adad854c37af39201ed8e0898164dcb8bd5e85a7
Alexander Olofsson <alexander.olofsson@liu.se>
2018-06-25 16:19:01 +0200

    Don't shadow exceptions from unattend controller

--- app/controllers/concerns/foreman_wds/unattended_controller_extensions.rb
+++ app/controllers/concerns/foreman_wds/unattended_controller_extensions.rb
@@ -1,21 +1,23 @@
 module ForemanWds
   module UnattendedControllerExtensions
     def render_template(kind)
-      return super unless kind == 'wds_localboot'
+      return super(kind) unless kind == 'wds_localboot'
 
-      iface = @host.provision_interface
+      begin
+        iface = @host.provision_interface
 
-      # Deploy regular DHCP and local boot TFTP
-      @host.provision_method = 'build'
-      @host.build = false
-      iface.send :rebuild_tftp
-      iface.send :rebuild_dhcp
+        # Deploy regular DHCP and local boot TFTP
+        @host.provision_method = 'build'
+        @host.build = false
+        iface.send :rebuild_tftp
+        iface.send :rebuild_dhcp
 
-      render inline: "Success. Local boot template was deployed successfully.\n"
-    rescue StandardError => e
-      message = format('Failed to set local boot template: %{error}', error: e)
-      logger.error message
-      render text: message, status: :error, content_type: 'text/plain'
+        render inline: "Success. Local boot template was deployed successfully.\n"
+      rescue StandardError => e
+        message = format('Failed to set local boot template: %{error}', error: e)
+        logger.error message
+        render text: message, status: :error, content_type: 'text/plain'
+      end
     end
   end
 end
